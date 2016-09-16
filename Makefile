5065cee83ec116bf6b890385e4ba404d965418ca
Anton Belyaev <xantbel@gmail.com>
2016-09-16 16:12:50 +0400

    MSPF-78: make logstash formatter (#1)
    * MSPF-78: make logstash formatter

---
+++ Makefile
@@ -0,0 +1,48 @@
+REBAR := $(shell which rebar3 2>/dev/null || which ./rebar3)
+SUBMODULES = build_utils
+SUBTARGETS = $(patsubst %,%/.git,$(SUBMODULES))
+
+SERVICE_NAME = dev
+UTILS_PATH := build_utils
+TEMPLATES_PATH = .
+
+BUILD_IMAGE_TAG := 6fb209e428feaa0ef6cec07d3909d8a3c4013537
+
+CALL_ANYWHERE := all submodules rebar-update compile lint xref test clean distclean
+
+CALL_W_CONTAINER := $(CALL_ANYWHERE)
+
+.PHONY: $(CALL_W_CONTAINER)
+
+all: compile
+
+-include $(UTILS_PATH)/make_lib/utils_container.mk
+
+$(SUBTARGETS): %/.git: %
+	git submodule update --init $<
+	touch $@
+
+submodules: $(SUBTARGETS)
+
+compile: submodules rebar-update
+	$(REBAR) compile
+
+rebar-update:
+	$(REBAR) update
+
+test: submodules
+	$(REBAR) eunit
+
+xref: submodules
+	$(REBAR) as test xref
+
+clean:
+	$(REBAR) clean
+
+distclean:
+	$(REBAR) clean -a
+	rm -rfv _build _builds _cache _steps _temp
+
+lint:
+	elvis rock
+
---
+++ src/lager_logstash_formatter.erl
@@ -0,0 +1,125 @@
+%%% @doc Public API, supervisor and application startup.
+%%% @end
+
+-module(lager_logstash_formatter).
+
+%% API
+-export([format/2]).
+-export([format/3]).
+
+
+%%
+%% API
+%%
+-spec format(lager_msg:lager_msg(), list(), _Colors) -> any().
+format(Msg, Config, _Colors) ->
+    format(Msg, Config).
+
+-spec format(lager_msg:lager_msg(), list()) -> any().
+format(Msg, _Config) ->
+    [jsx:encode(get_msg_map(Msg)), <<"\n">>].
+
+get_msg_map(Msg) ->
+    maps:merge(
+        get_metadata(Msg),
+        #{
+            '@timestamp' => get_timestamp(Msg),
+            '@severity'  => get_severity (Msg),
+            'message'    => get_message  (Msg)
+         }
+   ).
+
+-spec get_timestamp(lager_msg:lager_msg()) -> binary().
+get_timestamp(Msg) ->
+    {MegaSec, Sec, MicroSec} = lager_msg:timestamp(Msg),
+    USec = MegaSec * 1000000000000 + Sec * 1000000 + MicroSec,
+    {ok, TimeStamp} = rfc3339:format(USec, micro_seconds),
+    TimeStamp.
+
+-spec get_severity(lager_msg:lager_msg()) -> atom().
+get_severity(Msg) ->
+    lager_msg:severity(Msg).
+
+-spec get_message(lager_msg:lager_msg()) -> binary().
+get_message(Msg) ->
+    unicode:characters_to_binary(lager_msg:message(Msg), unicode).
+
+-spec get_metadata(lager_msg:lager_msg()) -> map().
+get_metadata(Msg) ->
+    case lager_msg:metadata(Msg) of
+        []   -> #{};
+        Else -> lists:foldl(fun add_meta/2, #{}, Else)
+    end.
+
+add_meta(MetaItem, Map) ->
+    {Key, Value} = printable(MetaItem),
+    Map#{Key => Value}.
+
+%% can't naively encode `File` or `Pid` as json as jsx see them as lists
+%% of integers
+printable({file, File}) ->
+    {file, unicode:characters_to_binary(File, unicode)};
+printable({pid, Pid}) ->
+    {pid, pid_list(Pid)};
+%% if a value is expressable in json use it directly, otherwise
+%% try to get a printable representation and express it as a json
+%% string
+printable({Key, Value}) when is_atom(Key); is_binary(Key) ->
+    case jsx:is_term(Value) of
+        true  -> {Key, Value};
+        false -> {Key, unicode:characters_to_binary(io_lib:format("~p", [Value]), unicode)}
+    end.
+
+pid_list(Pid) ->
+    try unicode:characters_to_binary(Pid, unicode) of
+        Pid0 -> Pid0
+    catch error:badarg ->
+            unicode:characters_to_binary(hd(io_lib:format("~p", [Pid])), unicode)
+    end.
+
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+%% {{MegaSec, Sec, MicroSec}, <<"1970-01-01T00:00:00Z">>}
+timestamp_now() ->
+    Now = os:timestamp(),
+    {MegaSec, Sec, MicroSec} = Now,
+    {ok, TimeStamp} = rfc3339:format(
+        MegaSec * 1000000000000 + Sec * 1000000 + MicroSec,
+        micro_seconds
+    ),
+    {Now, TimeStamp}.
+
+pid() ->
+    Self = self(),
+    {Self, unicode:characters_to_binary(io_lib:format("~p", [Self]), unicode)}.
+
+format_test_() ->
+    {Now, TimeStamp} = timestamp_now(),
+    {Self, Pid} = pid(),
+    [
+        {"basic message", ?_assertEqual(
+            [<<"{\"@severity\":\"info\",\"@timestamp\":\"",
+               TimeStamp/binary, "\",\"message\":\"hallo world\"}">>, <<"\n">>],
+            format(lager_msg:new("hallo world", Now, info, [], []), [])
+        )},
+        {"pid in metadata", ?_assertEqual(
+            [<<"{\"@severity\":\"info\",\"@timestamp\":\"", TimeStamp/binary,
+               "\",\"message\":\"hallo world\",\"pid\":\"", Pid/binary, "\"}">>, <<"\n">>],
+            format(lager_msg:new("hallo world", Now, info, [{pid, io_lib:format("~p", [Self])}], []), [])
+        )},
+        {"bare pid in metadata", ?_assertEqual(
+            [<<"{\"@severity\":\"info\",\"@timestamp\":\"", TimeStamp/binary,
+               "\",\"message\":\"hallo world\",\"pid\":\"<0.6.0>\"}">>, <<"\n">>],
+            format(lager_msg:new("hallo world", Now, info, [{pid, list_to_pid("<0.6.0>")}], []), [])
+        )},
+        {"file in metadata", ?_assertEqual(
+            [<<"{\"@severity\":\"info\",\"@timestamp\":\"", TimeStamp/binary,
+               "\",\"file\":\"foo.erl\",\"message\":\"hallo world\"}">>, <<"\n">>],
+            format(lager_msg:new("hallo world", Now, info, [{file, "foo.erl"}], []), [])
+        )}
+    ].
+
+-endif.
+
